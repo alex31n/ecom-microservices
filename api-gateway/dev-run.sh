0ea0dc31ab0677360f64f9f68a56b27089daaac5
Md Ashraful Islam <alex31n@gmail.com>
2024-05-25 23:20:15 +0600

    feature: add api gateway

---
+++ api-gateway/dev-run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# write permission to MAVEN_CONFIG dir
+#sudo chown -R 1000:/root/.m2
+
+# monitor project file, if modified then re-compile and run
+export TERM=xterm
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
+while true
+do
+  watch -d -t -g "ls -lR . | sha1sum" && mvn compile
+done
