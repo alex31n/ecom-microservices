9246bc4e5ec6bf537f82c81d0062a4ee76e4e9e4
Md Ashraful Islam <ashraful.islam@bracits.com>
2024-06-23 13:12:12 +0600

    convert dev-run to unix encode

--- product-service/dev-run.sh
+++ product-service/dev-run.sh
@@ -2,5 +2,5 @@
 export TERM=xterm
 mvn spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
 while true; do
-watch -d -t -g "ls -lR . | sha1sum" && mvn compile
+  watch -d -t -g "ls -lR . | sha1sum" && mvn compile
 done
